d3d5a201d513fc1063b327342075be9b183633cf
YangGray <52926582+yangtaeyoung93@users.noreply.github.com>
2021-11-18 01:07:26 +0900

    스프링 인터셉터 설정

---
+++ src/main/java/com/shop/mall/WebConfig.java
@@ -0,0 +1,26 @@
+package com.shop.mall;
+
+import com.shop.mall.web.interceptor.LogInterceptor;
+import com.shop.mall.web.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error");
+    }
+}
