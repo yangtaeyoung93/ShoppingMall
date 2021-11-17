5603f725a583d9e90adfc7ad845b12449ff835c0
YangGray <52926582+yangtaeyoung93@users.noreply.github.com>
2021-11-17 22:44:33 +0900

    Update OrderRepository.java

--- src/main/java/com/shop/mall/repository/OrderRepository.java
+++ src/main/java/com/shop/mall/repository/OrderRepository.java
@@ -102,9 +102,7 @@ public class OrderRepository {
 
         String jpql =  "select distinct o from Order o "+
                 "join fetch o.member m "+
-                "join fetch o.delivery d "+
-                "join fetch o.orderItems oi "+
-                "join fetch oi.item i ";
+                "join fetch o.delivery d ";
 
         boolean isFirstCondition = true;
 
