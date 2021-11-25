66f83b1831c14b17437e79a6c9db20b0303d48f9
YangGray <52926582+yangtaeyoung93@users.noreply.github.com>
2021-11-25 12:58:26 +0900

    sendRedirect 수정

--- src/main/java/com/shop/mall/web/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/shop/mall/web/interceptor/LoginCheckInterceptor.java
@@ -21,7 +21,7 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
         if(session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null){
             log.info("미인증 사용자 요청!!!");
 
-            response.sendRedirect("login?redirectURL=" + requestURI);
+            response.sendRedirect("/login);
             return false;
         }
         return true;
